98fc3eb12577eb95bf2e9c262b18f92e865e05e2
zzz-yu-ttkx <2545991413@qq.com>
2026-07-17 11:33:47 +0800

    0.2
    更改

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 8901,
+  },
   build: {
     rollupOptions: {
       external: ['/data/cards.js'],
